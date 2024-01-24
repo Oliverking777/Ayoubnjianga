45cece0d42e7c77019dd6ec512d3b2e54b35d601
Oliverking777 <150254158+Oliverking777@users.noreply.github.com>
2024-01-24 13:52:15 +0100

    OrderItem.java class

---
+++ src/main/java/com/ecommerceapp/ecommerceapp/productData.java
@@ -0,0 +1,70 @@
+package com.ecommerceapp.ecommerceapp;
+import java.sql.Date;
+
+
+public class productData {
+    private Integer Id;
+    private String productId;
+    private String productName;
+    private String type;
+    private Integer stock;
+    private Double price;
+    private String status;
+    private String image;
+    private Date date;
+    private Integer quantity;
+
+    public productData(Integer Id, String productId,
+                       String productName, String type, Integer stock,
+                       Double price, String status, Date date) {
+        this.Id = Id;
+        this.productId = productId;
+        this.productName = productName;
+        this.type = type;
+        this.stock = stock;
+        this.price = price;
+        this.status = status;
+        this.image = image;
+        this.date = date;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Integer getQuantity(){
+        return quantity;
+    }
+}
